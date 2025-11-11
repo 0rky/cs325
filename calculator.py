02c39472ad12678c437de0ebd896b43565fc83ec
0rky <manas.ork@gmail.com>
2025-11-11 16:38:34 -0700

    checking CICD

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ def subtract(x, y):
 
 def multiply(x, y):
     """Multiplies two numbers"""
-    return x * y
+    return x * 2y
 
 def divide(x, y):
     """Divides two numbers. Raises ValueError on division by zero."""
